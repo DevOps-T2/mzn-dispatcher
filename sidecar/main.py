113038738e0f0cfd6a076522b936cab085663610
troethe <thomas@43t.de>
2021-12-23 17:36:55 +0100

    Upload some meta information to the solution service

--- sidecar/main.py
+++ sidecar/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from typing import Optional
 
 import requests
 from pydantic import BaseModel
@@ -8,6 +9,9 @@ from kubernetes import client, config, watch
 
 namespace = "default"
 solution_file = "/src/solution.txt"
+errors_file = "/src/errors.txt"
+
+solver_container_index = 0  # The index of the container in the pod's container list
 
 solution_service = "solution-service"
 
@@ -17,27 +21,41 @@ user_id = os.environ["USER_ID"]
 logging.basicConfig(level=logging.INFO)
 
 
-class SolutionRequest(BaseModel):
+class SolutionInformationInput(BaseModel):
     user_id: str
     computation_id: str
-    body: str
+    status: Optional[str]
+    reason: Optional[str]
+    solver: Optional[str]
+    body: Optional[str]
 
 
-def save_solution():
+def save_solution(pod):
     logging.info("Detected solver finish")
-    if os.path.exists(solution_file):
-        with open(solution_file, "r") as fd:
-            data = SolutionRequest(user_id=user_id, computation_id=computation_id, body=fd.read()).dict()
+    solution_input = SolutionInformationInput(user_id=user_id, computation_id=computation_id)
 
-        headers = {'UserId': 'system', 'Role': 'admin'}
-        response = requests.post('http://'+solution_service+'/api/solutions/upload', json=data, headers=headers)
-        if response.status_code != 200:
-            logging.error("solution-service replied {}".format(response.text) +
-                          "On request {}".format(data))
+    if os.path.exists(errors_file) and os.stat(errors_file).st_size != 0:
+        with open(errors_file, "r") as fd:
+            solution_input.status = "Failure"
+            solution_input.reason = "Minizinc Error"
+            solution_input.body = fd.read()
+
+    elif os.path.exists(solution_file):
+        with open(solution_file, "r") as fd:
+            solution_input.status = "Success"
+            solution_input.body = fd.read()
+
+    solution_input.solver = pod.spec.containers[solver_container_index].image
+
+    headers = {'UserId': 'system', 'Role': 'admin'}
+    response = requests.post('http://'+solution_service+'/api/solutions/upload', json=solution_input.dict(), headers=headers)
+    if response.status_code != 200:
+        logging.error("solution-service replied {}".format(response.text) +
+                      "On request {}".format(solution_input.dict()))
 
 
 def solver_terminated(pod: client.V1Pod):
-    return pod.status.container_statuses[0].state.terminated
+    return pod.status.container_statuses[solver_container_index].state.terminated
 
 
 def main():
@@ -50,7 +68,7 @@ def main():
     pod = response.items[0]
 
     if solver_terminated(pod):
-        save_solution()
+        save_solution(pod)
         exit()
 
     w = watch.Watch()
@@ -58,7 +76,7 @@ def main():
     for event in w.stream(core_api.list_namespaced_pod, field_selector=field_selector, namespace=namespace, _continue=response.metadata._continue):
         pod = event["object"]
         if solver_terminated(pod):
-            save_solution()
+            save_solution(pod)
             w.stop()
             exit()
 
